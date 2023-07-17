6993e5f6fdb01f7e7c583939a59b37cd2a40320b
Artur Ispiriants <lezzi1994@gmail.com>
2023-07-17 14:37:07 +0300

    Add test MSIX projects.

---
+++ DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using Windows.Management.Deployment;
+using Microsoft.Win32;
+
+namespace DesktopClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                var currentPackage = Windows.ApplicationModel.Package.Current;
+                var version = currentPackage.Id.Version;
+
+                VersionTextBlock.Text = $"{version.Major}.{version.Minor}.{version.Revision}.{version.Build}";
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+
+        private async void UpdateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new OpenFileDialog { Filter = "MSIX bundle|*.msixbundle" };
+            if (filePicker.ShowDialog() != true)
+                return;
+            
+            var bundlePath = filePicker.FileName;
+            var packageManager = new PackageManager();
+            
+            await packageManager.AddPackageByUriAsync(new Uri(bundlePath, UriKind.Absolute), new AddPackageOptions
+            {
+                DeferRegistrationWhenPackagesAreInUse = true,
+                ForceTargetAppShutdown = false
+            });
+
+            ResultTextBlock.Text = "Ready to restart";
+        }
+    }
+}
